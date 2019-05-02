1d7a8b81c4dce1bea221358c7a387108e52f3abf
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-02 20:47:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
@@ -9,5 +9,56 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+  song = self.new
+   song.save
+   song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+   song = self.new
+   song.name = name
+   song.save
+   song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
 
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+        
+    #check if arg song exists. if it doesnt create a new song. if it does return that song name
+    # || means if "find by name" is falsey, then evaluate "create by name" against the argument and set it to the result. Equivalently, if "find by name" is truthy, then dont move on to evaluate "create by name" 
+  end
+    
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end  
+  
+  def self.new_from_filename(filename)
+    song = self.new
+    song.name = filename
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    song = self.new
+   song.name = filename
+   song.save
+   song
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
 end
